b3bde6556ff43a15be0096459aab093bf0332473
Noir-spec <67650694+Noir-spec@users.noreply.github.com>
2020-07-04 22:03:48 +0300

    Create wf-razorback-fomorians-kurulum.js

---
+++ komutlar/Warframe/wf-razorback-fomorians-kurulum.js
@@ -0,0 +1,46 @@
+const db = require('quick.db')
+const Discord = require('discord.js')
+const ayarlar = require('../ayarlar.json');
+
+exports.run = async(client, message, args) => {
+
+    var request = require('request');
+    request(`https://api.warframestat.us/pc/constructionProgress`, function(error, response, body) {
+
+        try {
+            var veri = JSON.parse(body);
+
+            let genel = veri
+
+
+            var embed = new Discord.RichEmbed()
+                .setDescription(`
+ID: ${genel.id}
+Fomorian İlerleme: ${genel.fomorianProgress}
+Razorback İlerleme: ${genel.razorbackProgress}
+Diğer İlerlemeler: ${genel.unknownProgress}
+`)
+                .setColor("BLUE")
+                .setFooter("Warframe")
+            message.channel.send(embed)
+        } catch (err) {
+            console.log(err);
+            message.reply(`**Warframe** yanıt vermıyor.`).then(msg => msg.delete(5000));
+        };
+    })
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ["w-rfkurulum"],
+    permLevel: 0,
+    kategori: "oyun",
+    oysistem: false
+};
+
+exports.help = {
+    name: 'warframe-razorback-fomorians-kurulum',
+    description: 'açıklama',
+    usage: `kullanım`
+};
