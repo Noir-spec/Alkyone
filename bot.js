e73ca9e917766d6a8b9f03bd25097b16edc412b1
Noir-spec <67650694+Noir-spec@users.noreply.github.com>
2020-07-04 21:57:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -149,3 +149,326 @@ client.on("error", e => {
 });
 
 client.login(ayarlar.token);
+
+//log sistemi
+
+//DİKKAT! BU KISMI BOT.JS VE YA İNDEX.JS HANGİSİ VARSA ONUN İÇİNE EN ALT KISMA ATACAKSIN
+
+
+client.on("guildMemberAdd", member => {
+
+    var user = member.user;
+    var tarih = ''
+    if (moment(user.createdAt).format('MM') === '01') {
+        var tarih = `${moment(user.createdAt).format('DD')} Ocak ${moment(user.createdAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.createdAt).format('MM') === '02') {
+        var tarih = `${moment(user.createdAt).format('DD')} Şubat ${moment(user.createdAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.createdAt).format('MM') === '03') {
+        var tarih = `${moment(user.createdAt).format('DD')} Mart ${moment(user.createdAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.createdAt).format('MM') === '04') {
+        var tarih = `${moment(user.createdAt).format('DD')} Nisan ${moment(user.createdAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.createdAt).format('MM') === '05') {
+        var tarih = `${moment(user.createdAt).format('DD')} Mayıs ${moment(user.createdAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.createdAt).format('MM') === '06') {
+        var tarih = `${moment(user.createdAt).format('DD')} Haziran ${moment(user.createdAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.createdAt).format('MM') === '07') {
+        var tarih = `${moment(user.createdAt).format('DD')} Temmuz ${moment(user.createdAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.createdAt).format('MM') === '08') {
+        var tarih = `${moment(user.createdAt).format('DD')} Ağustos ${moment(user.createdAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.createdAt).format('MM') === '09') {
+        var tarih = `${moment(user.createdAt).format('DD')} Eylül ${moment(user.createdAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.createdAt).format('MM') === '10') {
+        var tarih = `${moment(user.createdAt).format('DD')} Ekim ${moment(user.createdAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.createdAt).format('MM') === '11') {
+        var tarih = `${moment(user.createdAt).format('DD')} Kasım ${moment(user.createdAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.createdAt).format('MM') === '12') {
+        var tarih = `${moment(user.createdAt).format('DD')} Aralık ${moment(user.createdAt).format('YYYY HH:mm:ss')} `
+    }
+
+    var tarih2 = ''
+    if (moment(user.joinedAt).format('MM') === '01') {
+        var tarih2 = `${moment(user.joinedAt).format('DD')} Ocak ${moment(user.joinedAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.joinedAt).format('MM') === '02') {
+        var tarih2 = `${moment(user.joinedAt).format('DD')} Şubat ${moment(user.joinedAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.joinedAt).format('MM') === '03') {
+        var tarih2 = `${moment(user.joinedAt).format('DD')} Mart ${moment(user.joinedAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.joinedAt).format('MM') === '04') {
+        var tarih2 = `${moment(user.joinedAt).format('DD')} Nisan ${moment(user.joinedAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.joinedAt).format('MM') === '05') {
+        var tarih2 = `${moment(user.joinedAt).format('DD')} Mayıs ${moment(user.joinedAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.joinedAt).format('MM') === '06') {
+        var tarih2 = `${moment(user.joinedAt).format('DD')} Haziran ${moment(user.joinedAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.joinedAt).format('MM') === '07') {
+        var tarih2 = `${moment(user.joinedAt).format('DD')} Temmuz ${moment(user.joinedAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.joinedAt).format('MM') === '08') {
+        var tarih2 = `${moment(user.joinedAt).format('DD')} Ağustos ${moment(user.joinedAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.joinedAt).format('MM') === '09') {
+        var tarih2 = `${moment(user.joinedAt).format('DD')} Eylül ${moment(user.joinedAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.joinedAt).format('MM') === '10') {
+        var tarih2 = `${moment(user.joinedAt).format('DD')} Ekim ${moment(user.joinedAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.joinedAt).format('MM') === '11') {
+        var tarih2 = `${moment(user.joinedAt).format('DD')} Kasım ${moment(user.joinedAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.joinedAt).format('MM') === '12') {
+        var tarih2 = `${moment(user.joinedAt).format('DD')} Aralık ${moment(user.joinedAt).format('YYYY HH:mm:ss')} `
+    }
+
+    if (db.has(`log_${member.guild.id}`) === false) return;
+
+    var kanal = member.guild.channels.get(db.fetch(`log_${member.guild.id}`).replace("<#", "").replace(">", ""))
+    if (!kanal) return;
+
+    const embed = new Discord.RichEmbed()
+        .setColor(ayarlar.renk)
+        .setAuthor(`Sunucuya Bir Kullanıcı Katıldı!`, member.user.avatarURL)
+        .addField("Kullanıcı Tag", member.user.tag, true)
+        .addField("ID", member.user.id, true)
+        .addField("Discord Kayıt Tarihi", tarih, true)
+        .addField("Sunucuya Katıldığı Tarih", tarih2, true)
+        .setThumbnail(member.user.avatarURL)
+    kanal.send(embed);
+
+});
+
+client.on("guildMemberRemove", member => {
+
+    var user = member.user;
+    var tarih = ''
+    if (moment(user.createdAt).format('MM') === '01') {
+        var tarih = `${moment(user.createdAt).format('DD')} Ocak ${moment(user.createdAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.createdAt).format('MM') === '02') {
+        var tarih = `${moment(user.createdAt).format('DD')} Şubat ${moment(user.createdAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.createdAt).format('MM') === '03') {
+        var tarih = `${moment(user.createdAt).format('DD')} Mart ${moment(user.createdAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.createdAt).format('MM') === '04') {
+        var tarih = `${moment(user.createdAt).format('DD')} Nisan ${moment(user.createdAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.createdAt).format('MM') === '05') {
+        var tarih = `${moment(user.createdAt).format('DD')} Mayıs ${moment(user.createdAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.createdAt).format('MM') === '06') {
+        var tarih = `${moment(user.createdAt).format('DD')} Haziran ${moment(user.createdAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.createdAt).format('MM') === '07') {
+        var tarih = `${moment(user.createdAt).format('DD')} Temmuz ${moment(user.createdAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.createdAt).format('MM') === '08') {
+        var tarih = `${moment(user.createdAt).format('DD')} Ağustos ${moment(user.createdAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.createdAt).format('MM') === '09') {
+        var tarih = `${moment(user.createdAt).format('DD')} Eylül ${moment(user.createdAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.createdAt).format('MM') === '10') {
+        var tarih = `${moment(user.createdAt).format('DD')} Ekim ${moment(user.createdAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.createdAt).format('MM') === '11') {
+        var tarih = `${moment(user.createdAt).format('DD')} Kasım ${moment(user.createdAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.createdAt).format('MM') === '12') {
+        var tarih = `${moment(user.createdAt).format('DD')} Aralık ${moment(user.createdAt).format('YYYY HH:mm:ss')} `
+    }
+
+    var tarih2 = ''
+    if (moment(user.joinedAt).format('MM') === '01') {
+        var tarih2 = `${moment(user.joinedAt).format('DD')} Ocak ${moment(user.joinedAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.joinedAt).format('MM') === '02') {
+        var tarih2 = `${moment(user.joinedAt).format('DD')} Şubat ${moment(user.joinedAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.joinedAt).format('MM') === '03') {
+        var tarih2 = `${moment(user.joinedAt).format('DD')} Mart ${moment(user.joinedAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.joinedAt).format('MM') === '04') {
+        var tarih2 = `${moment(user.joinedAt).format('DD')} Nisan ${moment(user.joinedAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.joinedAt).format('MM') === '05') {
+        var tarih2 = `${moment(user.joinedAt).format('DD')} Mayıs ${moment(user.joinedAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.joinedAt).format('MM') === '06') {
+        var tarih2 = `${moment(user.joinedAt).format('DD')} Haziran ${moment(user.joinedAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.joinedAt).format('MM') === '07') {
+        var tarih2 = `${moment(user.joinedAt).format('DD')} Temmuz ${moment(user.joinedAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.joinedAt).format('MM') === '08') {
+        var tarih2 = `${moment(user.joinedAt).format('DD')} Ağustos ${moment(user.joinedAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.joinedAt).format('MM') === '09') {
+        var tarih2 = `${moment(user.joinedAt).format('DD')} Eylül ${moment(user.joinedAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.joinedAt).format('MM') === '10') {
+        var tarih2 = `${moment(user.joinedAt).format('DD')} Ekim ${moment(user.joinedAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.joinedAt).format('MM') === '11') {
+        var tarih2 = `${moment(user.joinedAt).format('DD')} Kasım ${moment(user.joinedAt).format('YYYY HH:mm:ss')} `
+    }
+    if (moment(user.joinedAt).format('MM') === '12') {
+        var tarih2 = `${moment(user.joinedAt).format('DD')} Aralık ${moment(user.joinedAt).format('YYYY HH:mm:ss')} `
+    }
+
+    if (db.has(`log_${member.guild.id}`) === false) return;
+
+    var kanal = member.guild.channels.get(db.fetch(`log_${member.guild.id}`).replace("<#", "").replace(">", ""))
+    if (!kanal) return;
+
+    const embed = new Discord.RichEmbed()
+        .setColor(ayarlar.renk)
+        .setAuthor(`Sunucudan Bir Kullanıcı Ayrıldı!`, member.user.avatarURL)
+        .addField("Kullanıcı Tag", member.user.tag, true)
+        .addField("ID", member.user.id, true)
+        .addField("Discord Kayıt Tarihi", tarih, true)
+        .addField("Sunucuya Katıldığı Tarih", tarih2, true)
+        .setThumbnail(member.user.avatarURL)
+    kanal.send(embed);
+
+});
+
+client.on("messageDelete", message => {
+
+    if (message.author.bot) return;
+
+    var user = message.author;
+
+    if (db.has(`log_${message.guild.id}`) === false) return;
+
+    var kanal = message.guild.channels.get(db.fetch(`log_${message.guild.id}`).replace("<#", "").replace(">", ""))
+    if (!kanal) return;
+
+    const embed = new Discord.RichEmbed()
+        .setColor(ayarlar.renk)
+        .setAuthor(`Bir Mesaj Silindi!`, message.author.avatarURL)
+        .addField("Kullanıcı Tag", message.author.tag, true)
+        .addField("ID", message.author.id, true)
+        .addField("Silinen Mesaj", "```" + message.content + "```")
+        .setThumbnail(message.author.avatarURL)
+    kanal.send(embed);
+
+});
+
+client.on("messageUpdate", async(oldMsg, newMsg) => {
+
+    if (oldMsg.author.bot) return;
+
+
+    var user = oldMsg.author;
+
+
+    if (db.has(`log_${oldMsg.guild.id}`) === false) return;
+
+    var kanal = oldMsg.guild.channels.get(db.fetch(`log_${oldMsg.guild.id}`).replace("<#", "").replace(">", ""))
+    if (!kanal) return;
+
+    const embed = new Discord.RichEmbed()
+        .setColor(ayarlar.renk)
+        .setAuthor(`Bir Mesaj Düzenlendi!`, oldMsg.author.avatarURL)
+        .addField("Kullanıcı Tag", oldMsg.author.tag, true)
+        .addField("ID", oldMsg.author.id, true)
+        .addField("Eski Mesaj", "```" + oldMsg.content + "```")
+        .addField("Yeni Mesaj", "```" + newMsg.content + "```")
+        .setThumbnail(oldMsg.author.avatarURL)
+    kanal.send(embed);
+
+});
+
+client.on("roleCreate", role => {
+
+
+    if (db.has(`log_${role.guild.id}`) === false) return;
+
+    var kanal = role.guild.channels.get(db.fetch(`log_${role.guild.id}`).replace("<#", "").replace(">", ""))
+    if (!kanal) return;
+
+    const embed = new Discord.RichEmbed()
+        .setColor(ayarlar.renk)
+        .setAuthor(`Bir Rol Oluşturuldu!`, role.guild.iconURL)
+        .addField("Rol", `\`${role.name}\``, true)
+        .addField("Rol Rengi Kodu", `${role.hexColor}`, true)
+    kanal.send(embed);
+
+});
+
+client.on("roleDelete", role => {
+
+
+    if (db.has(`log_${role.guild.id}`) === false) return;
+
+    var kanal = role.guild.channels.get(db.fetch(`log_${role.guild.id}`).replace("<#", "").replace(">", ""))
+    if (!kanal) return;
+
+    const embed = new Discord.RichEmbed()
+        .setColor(ayarlar.renk)
+        .setAuthor(`Bir Rol Kaldırıldı!`, role.guild.iconURL)
+        .addField("Rol", `\`${role.name}\``, true)
+        .addField("Rol Rengi Kodu", `${role.hexColor}`, true)
+    kanal.send(embed);
+
+});
+
+client.on("roleUpdate", role => {
+
+
+    if (db.has(`log_${role.guild.id}`) === false) return;
+
+    var kanal = role.guild.channels.get(db.fetch(`log_${role.guild.id}`).replace("<#", "").replace(">", ""))
+    if (!kanal) return;
+
+    const embed = new Discord.RichEmbed()
+        .setColor(ayarlar.renk)
+        .setAuthor(`Bir Rol Güncellendi!`, role.guild.iconURL)
+        .addField("Rol", `\`${role.name}\``, true)
+        .addField("Rol Rengi Kodu", `${role.hexColor}`, true)
+    kanal.send(embed);
+
+});
+
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+
+
+    if (db.has(`log_${oldMember.guild.id}`) === false) return;
+
+    var kanal = oldMember.guild.channels.get(db.fetch(`log_${oldMember.guild.id}`).replace("<#", "").replace(">", ""))
+    if (!kanal) return;
+
+    let newUserChannel = newMember.voiceChannel
+    let oldUserChannel = oldMember.voiceChannel
+
+    if (oldUserChannel === undefined && newUserChannel !== undefined) {
+
+        const embed = new Discord.RichEmbed()
+            .setColor(ayarlar.renk)
+            .setDescription(`${newMember.user.tag} adlı kullanıcı \`${newUserChannel.name}\` isimli sesli kanala giriş yaptı!`)
+        kanal.send(embed);
+
+    } else if (newUserChannel === undefined) {
+
+        const embed = new Discord.RichEmbed()
+            .setColor(ayarlar.renk)
+            .setDescription(`${newMember.user.tag} adlı kullanıcı bir sesli kanaldan çıkış yaptı!`)
+        kanal.send(embed);
+
+    }
+});
